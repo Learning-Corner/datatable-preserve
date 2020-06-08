301af40d4a7778ae555d29ce764188f67ed62cac
ANDRZE7 <andrzej.gagola@gmail.com>
2020-06-08 20:47:46 +0200

    Initial commit.

---
+++ src/main/java/com/example/datatablepreserv/controllers/Main.java
@@ -0,0 +1,14 @@
+package com.example.datatablepreserv.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Main {
+
+    @GetMapping({"", "index"})
+    String index() {
+        return "index";
+    }
+
+}
