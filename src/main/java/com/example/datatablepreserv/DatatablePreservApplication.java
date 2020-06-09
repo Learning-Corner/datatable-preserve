4eb0680daf05183e092d8c998aa870ceb4ebf01b
ANDRZE7 <andrzej.gagola@gmail.com>
2020-06-09 06:19:17 +0200

    Added js, css, favico, index.html, app scheleton.

---
+++ src/main/java/com/example/datatablepreserv/DatatablePreservApplication.java
@@ -0,0 +1,13 @@
+package com.example.datatablepreserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatablePreservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatatablePreservApplication.class, args);
+	}
+
+}
